28ae372d5db0c34ce8bd4e50a8d7f1e8d2260c0a
joaofelipe <joaofelipe1294@gmail.com>
2025-03-26 07:58:20 -0300

    feat: add cache write after create a new record

--- app/models/film.rb
+++ app/models/film.rb
@@ -4,4 +4,21 @@ class Film < ApplicationRecord
   has_many :rentals, through: :inventories
 
   belongs_to :language
+
+  after_save :write_cache
+
+  private
+
+  def write_cache
+    Rails.cache.write(
+      "/api/v1/films",
+      Film
+        .all
+        .map { |film| Api::V1::FilmPresenter.new(film).to_json }
+        .append(expiration_key: "#{Film.count}-#{updated_at}"
+      )
+    )
+
+    true
+  end
 end
